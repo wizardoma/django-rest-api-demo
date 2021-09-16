8e7418181c31c6b8fa04d1344300d07385224212
Ibekason Alexander <alibekason@gmail.com>
2021-09-16 05:56:30 +0100

    added viewsets to api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.views import Response
-
-from profiles_api.response import JsonResponse
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         an_apiview = [
             'Uses a Http methods as functions',
@@ -12,4 +15,28 @@ class HelloApiView(APIView):
             'Gives you the most control'
         ]
 
-        return Response(data=JsonResponse(is_error=False, status=200, data=an_apiview, error_message=None))
+        return Response({'data': an_apiview})
+
+    def post(self, request):
+        serializers = self.serializer_class(data=request.data)
+        if serializers.is_valid():
+            name = serializers.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message': message})
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST )
+
+
+class HelloViewSet(ViewSet):
+
+    def list(self, request):
+        a_viewset = [
+            'Hello'
+        ]
+
+        return Response({'message': a_viewset})
+
+    def retrieve(self, request, pk=None):
+        return Response({'done': 'YEs o'})
+
